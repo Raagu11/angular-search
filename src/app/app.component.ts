6b26e16c91a4d480f2cf63ff8434836d9efe6884
Raagu11 <106811919+Raagu11@users.noreply.github.com>
2022-10-09 20:43:57 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component, VERSION,OnInit } from '@angular/core';
+interface Employee{
+  name:string
+  salary:number
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+
   name = 'Angular ' + VERSION.major;
+  searchText: string;
+
+employeeList:Employee[] =[
+  {
+  name:'raagu',
+  salary:50000
+},
+  {
+    name:'baagu',
+  salary:40000,
+}];
+ngOnInit():void{
+this.search();
 }
+
+searchKey(data:string){
+  this.searchText = data;
+  this.search();
+}
+
+search(){
+  this.employeeList = this.searchText ==="" ? :  this.employeeList
+  .filter((element)=>{
+    return element.name.toLowerCase()
+  })
+}
+}
+
+
+
