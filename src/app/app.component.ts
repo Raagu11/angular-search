d3c545c9cf947dfed69a0f91e3dff177a9f74ab5
Raagu11 <106811919+Raagu11@users.noreply.github.com>
2022-10-09 20:50:22 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,43 @@
-import { Component, VERSION,OnInit } from '@angular/core';
-interface Employee{
-  name:string
-  salary:number
+import { Component, VERSION, OnInit } from '@angular/core';
+interface Employee {
+  name: string;
+  salary: number;
 }
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   name = 'Angular ' + VERSION.major;
   searchText: string;
 
-employeeList:Employee[] =[
-  {
-  name:'raagu',
-  salary:50000
-},
-  {
-    name:'baagu',
-  salary:40000,
-}];
-ngOnInit():void{
-this.search();
+  employeeList: Employee[] = [
+    {
+      name: 'raagu',
+      salary: 50000,
+    },
+    {
+      name: 'baagu',
+      salary: 40000,
+    },
+  ];
+
+  ngOnInit(): void {
+    this.search();
+  }
+
+  searchKey(data: string) {
+    this.searchText = data;
+    this.search();
+  }
+
+  search() {
+    this.employeeList =
+      this.searchText === ''
+        ? this.employeeList
+        : this.employeeList.filter((element) => {
+            return element.name.toLowerCase();
+          });
+  }
 }
-
-searchKey(data:string){
-  this.searchText = data;
-  this.search();
-}
-
-search(){
-  this.employeeList = this.searchText ==="" ? :  this.employeeList
-  .filter((element)=>{
-    return element.name.toLowerCase()
-  })
-}
-}
-
-
-
